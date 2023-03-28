5ac6f0b81a1d0e0ce3b66ab75df05bef5d037cd9
Anthony Lopez <anthonydlr66@gmail.com>
2023-03-28 11:45:16 -0400

    Cat Arrays

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+
+
+function destructivelyAppendCat(string) {
+    cats.push(string);
+}
+function destructivelyPrependCat(string) {
+    cats.unshift(string);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+function appendCat(name) {
+    const copyCat = [...cats];
+    copyCat.push(name);
+    return copyCat;
+}
+function prependCat(name) {
+    const copyCat = [name, ...cats];
+    return copyCat;
+}
+function removeLastCat(name) {
+    const copyCat = [...cats];
+    copyCat.pop(name)
+    return copyCat
+}
+function removeFirstCat(name) {
+    const copyCat = [...cats];
+    copyCat.shift(name)
+    return copyCat
+}
